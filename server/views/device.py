0b24fc8e904778966b22435912f0ba21504ab3a2
tvpluss <truongphuocst310301@gmail.com>
2022-04-19 18:45:15 +0700

    prepare device.views code

--- server/views/device.py
+++ server/views/device.py
@@ -3,14 +3,14 @@ from flask import request, jsonify
 from bson import ObjectId
 from models.deviceModel import deviceModel  # call model file
 # from bson.objectid import ObjectId # Allow using ObjectId
-# from factory.adafruit import ADA
+from factory.adafruit import ADA
 
 from flask_jwt_extended import jwt_required
 
 devices = deviceModel()
-ada = 'ADA()'
+# ada = ADA()
 class deviceAPI(MethodView):
-    decorators = [jwt_required()]
+    # decorators = [jwt_required()]
     def get(self):
         device_id = request.args.get('device_id')
         if device_id == None:
@@ -26,8 +26,9 @@ class deviceAPI(MethodView):
 
     def put(self, device_id):
         device = devices.find_by_id(ObjectId(device_id))
-        print(device['name'], device['feed'])
-        newvalue = "1" if device['feed'] == '0' else "0"
-        ada.update('fan', newvalue)
+        value = request.form['value']
+        print(device['name'], "with new value= " +str(value))
+        # newvalue = "1" if device['feed'] == '0' else "0"
+        # ada.update('fan', newvalue)
         # if device['feed'] == 1:
         return device, 201
