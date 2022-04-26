dca1c728978855ee0354510f510e666c16cb3fb4
Nguyễn Thành Đạt <thanhdat.dev.js@gmail.com>
2022-04-26 12:23:54 +0700

    integrate API

--- app/navigation/index.tsx
+++ app/navigation/index.tsx
@@ -97,21 +97,6 @@ function BottomTabNavigator() {
           tabBarIcon: ({ color }) => (
             <FontAwesome name="home" size={25} color={color} />
           ),
-          // headerRight: () => (
-          //   <Pressable
-          //     onPress={() => navigation.navigate("Modal")}
-          //     style={({ pressed }) => ({
-          //       opacity: pressed ? 0.5 : 1,
-          //     })}
-          //   >
-          //     <FontAwesome
-          //       name="info-circle"
-          //       size={25}
-          //       color={Colors[colorScheme].text}
-          //       style={{ marginRight: 15 }}
-          //     />
-          //   </Pressable>
-          // ),
         })}
       />
       <BottomTab.Screen
